38618798493abe3a5a4eadbe31bbbc5ad3240ec8
rachid <rbourigue@gmail.com>
2022-03-26 09:43:40 +0100

    first commit

---
+++ index.py
@@ -0,0 +1,33 @@
+import sys
+
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+from main import Ui_MainWindow
+
+
+class MainApp (QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+         super (MainApp, self).__init__(parent)
+         self.setupUi(self)
+         self.pushButton_2.clicked.connect(self.prin)
+    def prin(self):
+        self.lineEdit_2.setText("hello rachid")
+
+
+
+
+
+
+
+
+
+
+def main():
+
+   app = QApplication(sys.argv)
+   myWin = MainApp()
+   myWin.show()
+   sys.exit(app.exec_())
+if __name__ == '__main__':
+    main()
+
